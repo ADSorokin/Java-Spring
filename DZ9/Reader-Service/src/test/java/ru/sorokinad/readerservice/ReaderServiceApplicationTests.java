0ca47500e98ded2d1519c697f60fb55f5360ea79
alexds <alexsksd@gmail.com>
2024-12-22 21:25:48 +0300

    DZ9

---
+++ DZ9/Reader-Service/src/test/java/ru/sorokinad/readerservice/ReaderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sorokinad.readerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ DZ9/src/test/java/ru/sorokinad/dz9/Dz9ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sorokinad.dz9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dz9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
